35f22401e7a0146167705fd78dfcea36de024f8d
Aleks4920 <49626050+Aleks4920@users.noreply.github.com>
2022-02-03 13:32:05 -0500

    started poroject, added API

---
+++ src/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta name="robots" content="noindex, nofollow">
+        <meta charset="utf-8">
+        <script type="text/javascript" src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+        <script type="text/javascript" src="./js/main.js"></script>
+        <title>Calories</title>
+    </head>
+
+</html>
---
+++ src/js/main.js
@@ -0,0 +1,18 @@
+
+
+// import api
+
+var query = '3lb carrots and a chicken sandwich'
+$.ajax({
+    method: 'GET',
+    url: 'https://api.calorieninjas.com/v1/nutrition?query=' + query,
+    headers: { 'X-Api-Key': 'WsuXZ4DxNiOMxogK3U7xig==zR210Rbuk8WIDnJK'},
+    contentType: 'application/json',
+    success: function(result) {
+        console.log(result);
+    },
+    error: function ajaxError(jqXHR) {
+        console.error('Error: ', jqXHR.responseText);
+    }
+});
+
