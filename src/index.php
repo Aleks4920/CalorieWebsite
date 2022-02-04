219df153bc37fd502a45eee345afdf5c00a3eca5
Aleks4920 <49626050+Aleks4920@users.noreply.github.com>
2022-02-03 20:37:04 -0500

    Added basic progress circle & php script

--- src/index.php
+++ src/index.php
@@ -3,13 +3,44 @@
     <head>
         <meta name="robots" content="noindex, nofollow">
         <meta charset="utf-8">
+        <script src='./config.js'></script>
         <script type="text/javascript" src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
         <script type="text/javascript" src="./js/main.js"></script>
+        <link rel="stylesheet" href="css/styles.css">
         <title>Calories</title>
     </head>
     <body>
+
+        <div class="wrapper">
+            <div class="loader center"><span></span></div>
+            
+        </div>
+
+        <script>
+            $(window).on('load', function(){
+                $(".wrapper").fadeOut("slow");
+            })
+        </script>
+
+
         <h1>Calories</h1>
-        <p id="food">
+        <p id="calories">
+
+        <div role="progressbar" aria-valuenow="99" aria-valuemin="0" aria-valuemax="6000" style="--filled: 60; --number:1500"></div>
+        <?php
+            require 'db.php';
+
+            // set up & run query
+            $sql = "SELECT * FROM relatives";
+            $cmd = $db->prepare($sql);
+            $cmd->execute();
+            $relatives = $cmd->fetchAll();
+
+
+            $db = null;
+        ?>
+
+
 
         
 
--- src/js/main.js
+++ src/js/main.js
@@ -6,11 +6,11 @@ var query = '3lb carrots'
 $.ajax({
     method: 'GET',
     url: 'https://api.calorieninjas.com/v1/nutrition?query=' + query,
-    headers: { 'X-Api-Key': 'WsuXZ4DxNiOMxogK3U7xig==zR210Rbuk8WIDnJK'},
+    headers: { 'X-Api-Key': process.env.APIKEY},
     contentType: 'application/json',
     success: function(result) {
         console.log(result);
-        document.getElementById("food").innerHTML = JSON.stringify(result.items[0].calories);
+        document.getElementById("calories").innerHTML = JSON.stringify(result.items[0].calories);
 
         // document.getElementById("food").innerHTML = JSON.parse(result);
 
