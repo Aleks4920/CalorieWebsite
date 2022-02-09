626580fdf6e314c04be425a0bfdeb51101d085c8
Aleks4920 <49626050+Aleks4920@users.noreply.github.com>
2022-02-09 17:30:11 -0500

    added depug function

--- src/index.php
+++ src/index.php
@@ -21,61 +21,52 @@
             })
         </script>
 
+        
+
 
         <h1>Calories</h1>
         <p id="calories"></p>
 
+        <form method="post" action="addCalories.php">
+            <select name="add" id="add">
+                <option value="food">Add Food</option>
+                <option value="calories">Add calorie Amount</option>
+            </select>
+
+            <input type="text" id="foodCalories" name="foodCalories">
+            <input type="submit" value="Add">
+        </form>
         <div role="progressbar" aria-valuenow="99" aria-valuemin="0" aria-valuemax="6000" style="--filled: 60; --number:1500"></div>
         
         
         <?php
             require 'db.php';
 
+
+            function debug_to_console($data) {
+                $output = $data;
+                if (is_array($output))
+                    $output = implode(',', $output);
             
-            // set up & run query
-            $sql = "SELECT * FROM relatives";
-            $cmd = $db->prepare($sql);
-            $cmd->execute();
-            $relatives = $cmd->fetchAll();
-                
-
-            $APIURL = 'https://api.calorieninjas.com/v1/nutrition?query=';
-            $query = array('3lb carrots and a chicken sandwich');
-            $token= 'WsuXZ4DxNiOMxogK3U7xig==zR210Rbuk8WIDnJK';
-            
-
-            
-            $curl = curl_init();
-
-            curl_setopt_array($curl, [
-            CURLOPT_URL => "https://calorieninjas.p.rapidapi.com/v1/nutrition?query=1%20glass%20of%20skimmed%20milk.%20",
-            CURLOPT_RETURNTRANSFER => true,
-            CURLOPT_FOLLOWLOCATION => true,
-            CURLOPT_ENCODING => "",
-            CURLOPT_MAXREDIRS => 10,
-            CURLOPT_TIMEOUT => 30,
-            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-            CURLOPT_CUSTOMREQUEST => "GET",
-            CURLOPT_HTTPHEADER => [
-                "x-rapidapi-host: calorieninjas.p.rapidapi.com",
-                "x-rapidapi-key: ce19d0164fmsh3d383efc0e85ce5p16dcb1jsnb1a4a3c79541"
-            ],
-            ]);
-
-            $response = curl_exec($curl);
-            $err = curl_error($curl);
-
-            curl_close($curl);
-
-            if ($err) {
-            echo "cURL Error #:" . $err;
-            } else {
-                json_decode($response);
-                echo $response;
+                echo "<script>console.log('Debug Objects: " . $output . "' );</script>";
             }
             
+            // set up & run query
+            $sql = 'SELECT userID, Calories, CalorieGoal FROM CalorieAppUsers where name = "Kyle";';
+            $cmd = $db->prepare($sql);
+            $cmd->execute();
+            $results = $cmd->fetchAll();
+            debug_to_console($results);
+            foreach($results as $result){
+                foreach($result as $i){
+                    echo $i . "\n";
+                }
+                
+            }
+            // $precent = ($calories / $calorieGoal) * 100;
+            // echo $precent;
 
-
+            
             $db = null;
         ?>
         
