9eb490c6dd7bafde60ca14ee0c8d9b603c835a17
Aleks4920 <49626050+Aleks4920@users.noreply.github.com>
2022-02-09 20:54:50 -0500

    can now update users calories based on input, And added comments

--- src/addCalories.php
+++ src/addCalories.php
@@ -3,36 +3,51 @@
     <head>
         <meta charset="UTF-8" />
         <title>Adding calories...</title>
-        <meta http-equiv = "refresh" content = "1; url = index.php" />
     </head>
     <body>
         
+        
+
         <?php
 
-
+            // get all data from form submission
             $dataType = $_POST['add'];
             $input = $_POST['foodCalories'];
-
+            $name = $_POST['name'];
             
 
-
+            // check input to see if user is inputing a food or specific calorie amount
             if ($dataType == 'calories'){
-
-
+                // make sure that input is a validnumber
+                if (is_numeric($input)){
+                    $calories = $input;
+                }
+                else{
+                    throw new Exception("invalid input");
+                    exit();
+                }
+                    
             }elseif ($dataType == 'food'){
+                if (is_numeric($input)){
+                    throw new Exception("invalid input");
+                    break;
+                }
 
+                // change string to an array
                 $delimiter = ' ';
                 $query = "";
                 $words = explode($delimiter, $input);
                 
+                // add array to query string
                 foreach($words as $word){
                     $query = $query . $word . '%20';
                 }
-
-                
-
+                    
+                // use curl to send request to api
                 $curl = curl_init();
 
+
+                // set up api call 
                 curl_setopt_array($curl, [
                     CURLOPT_URL => "https://calorieninjas.p.rapidapi.com/v1/nutrition?query=" . $query,
                     CURLOPT_RETURNTRANSFER => true,
@@ -47,21 +62,42 @@
                         "x-rapidapi-key: ce19d0164fmsh3d383efc0e85ce5p16dcb1jsnb1a4a3c79541"
                     ],
                 ]);
-
+                
                 $response = curl_exec($curl);
                 $err = curl_error($curl);
 
                 curl_close($curl);
-
+                // if api request is successful decode the json and parse out calories
                 if ($err) {
-                    echo "cURL Error #:" . $err;
+                    throw new Exception("cURL Error #:". $err);
+                    exit();
                 } else {
-                    echo $response;
+                    $data = json_decode($response);
+                    $calories = $data->items[0]->calories;
                 }
 
+                
+
+                
+
+                
             }
+            
+            // attempt to update users calorie amount from the data above
+            // if anything failed the program will trow an error and exit
+            try {
+                require 'db.php';
 
+                echo $calories;
 
+                $sql = 'UPDATE CalorieAppUsers set Calories = Calories +' . $calories . ' where name = "' . $name . '";';
+                $cmd = $db->prepare($sql);
+                $cmd->execute();
+            } catch {
+                throw new Exception('Cant update database');
+                exit();
+            }
+            
 
             
 
@@ -69,6 +105,16 @@
 
 
         ?>
+
+        <form method="post" action="index.php" name="passName">
+            <?php echo '<input type="hidden" id="name" name="name" value="' . $name . '">'?>
+        </form>
+
+        <script>
+            window.onload = function(){
+                document.forms['passName'].submit();
+                }
+        </script>
 
     </body>
 </html>
--- src/index.php
+++ src/index.php
@@ -10,26 +10,40 @@
     </head>
     <body>
 
+
+        <!-- loader that plays while page is loading, credit to Nobuaki Honma-->
+        <!-- codepen at https://codepen.io/nobuakihonma/pen/dYbqLQ  -->
         <div class="wrapper">
-            <div class="loader center"><span></span></div>
-            
+            <ul class="loader-list">
+                <li>
+                <div class="loader-5 center"><span></span></div>
+            </li>
+            </ul>
         </div>
 
-        
+        <!-- makes loader disappear after page is loaded-->
+        <script>
+            $(window).on('load', function(){
+                $(".wrapper").fadeOut("fast");
+            })
+        </script>
 
-        
-
+        <!-- get the users -->
+        <?php
+            // $name = $_POST['name'];
+            $name = "Philep";
+            ?>
 
         <h1>Calories</h1>
-        <p id="calories"></p>
-
+            
+        <!-- a form for the user to add their calories or what they ate -->
         <form method="post" action="addCalories.php">
             <select name="add" id="add">
                 <option value="food">Add Food</option>
                 <option value="calories">Add calorie Amount</option>
             </select>
-
             <input type="text" id="foodCalories" name="foodCalories">
+            <?php echo '<input type="hidden" id="name" name="name" value="' . $name . '">'?>
             <input type="submit" value="Add">
         </form>
         
@@ -37,23 +51,26 @@
 
         
         <?php
+        
             require 'db.php';
             
-            // set up & run query
-            $sql = 'SELECT Calories, CalorieGoal FROM CalorieAppUsers where name = "Philep";';
+            // set up & run query to get users calories and calorie goal
+            $sql = 'SELECT Calories, CalorieGoal FROM CalorieAppUsers where name = "' . $name . '";';
             $cmd = $db->prepare($sql);
             $cmd->execute();
             $results = $cmd->fetchAll(PDO::FETCH_ASSOC);
 
-
+            // get values from the table returned
             $calories = $results[0]['Calories'];
             $calorieGoal = $results[0]['CalorieGoal'];
             
-
+            // calculate how much of the circle to fill (how close the user is to their goal)
             $precent = ($calories / $calorieGoal) * 100;
             
-
+            // show users progress using a circle that fills up as they get closer to their goal
             echo '<div role="progressbar" aria-valuenow="99" aria-valuemin="0" aria-valuemax="6000" style="--filled: '. $precent . '; --number:'. $calories . '"></div>';
+            
+            // exit data base
             $db = null;
         ?>
 
@@ -61,11 +78,7 @@
         
 
 
-        <script>
-            $(window).on('load', function(){
-                $(".wrapper").fadeOut("slow");
-            })
-        </script>
+        
 
     </body>
 
